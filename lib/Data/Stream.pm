d8f24919fec784c08409dc010874807236e3f9c3
mf-ka-sato <ka-sato@mfac.jp>
2018-05-22 23:31:41 +0900

    filter, take_whileを追加

--- lib/Data/Stream.pm
+++ lib/Data/Stream.pm
@@ -34,4 +34,30 @@ sub map {
     Data::Stream->new($conf, $self->{cons});
 }
 
+sub filter {
+    my ($self, $f) = @_;
+    my $conf = clone $self->{config};
+
+    if (defined $conf->{filter_f}) {
+        $conf->{filter_f} = sub { $conf->{filter_f}->($_[0]) && $f->($_[0]) }
+    } else {
+        $conf->{filter_f} = $f;
+    }
+
+    Data::Stream->new($conf, $self);
+}
+
+sub take_while {
+    my ($self, $f) = @_;
+    my $conf = clone $self->{config};
+
+    if (defined $conf->{take_while_f}) {
+        $conf->{take_while_f} = sub { $conf->{take_while_f}->($_[0]) && $f->($_[0]) }
+    } else {
+        $conf->{take_while_f} = $f;
+    }
+
+    Data::Stream->new($conf, $self);
+}
+
 1;
