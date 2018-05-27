01d4a0a2486d56a0c166abdb509ae94a7ba710bb
mf-ka-sato <ka-sato@mfac.jp>
2018-05-27 21:37:52 +0900

    Lazy追加

---
+++ lib/Monad/Lazy.pm
@@ -0,0 +1,48 @@
+package Monad::Lazy;
+use strict;
+use warnings;
+use utf8;
+use Mouse;
+
+# properties
+
+has _value => (
+    is  => 'rw',
+    default => undef
+);
+
+has _gen => (
+    is  => 'ro',
+    isa => 'CodeRef'
+);
+
+# Monad Functions
+
+sub mreturn {
+    my $v = shift;
+    Monad::Lazy->new(_value => $v, _gen => sub { $v });
+}
+
+# methods
+
+sub eval {
+    my $self = shift;
+
+    $self->_value($self->_gen->()) if not defined $self->_value;
+    $self->_value;
+}
+
+sub map {
+    my ($self, $f) = @_;
+    Monad::Lazy->new(_gen => sub { $f->($self->eval) });
+}
+
+sub flatmap {
+    my ($self, $f) = @_;
+    # $f :: a -> Monad::Lazy(a)
+
+    Monad::Lazy->new(_gen => sub { $f->($self->eval)->eval });
+}
+
+
+1;
